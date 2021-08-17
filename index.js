dd553eaf189d31d1a8822800e6d5775156a025f8
IZanko <zankoiva@hotmail.com>
2021-08-17 13:58:14 -0400

    install passport, create /login endpoint, update documentation

--- index.js
+++ index.js
@@ -20,19 +20,29 @@ const app = express();
 
 app.use(bodyParser.json());
 
+/*importing auth.js into project*/
+let auth = require("./auth")(app);
+/*require and import passport.js*/
+const passport = require("passport");
+require("./passport");
+
 let message = "";
 
 //  Return a list of ALL movies to the user
-app.get("/movies", (req, res) => {
-  Movies.find()
-    .then(movie => {
-      res.json(movie);
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
+app.get(
+  "/movies",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Movies.find()
+      .then(movie => {
+        res.json(movie);
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 // Return data about a single movie by title to the user
 app.get("/movies/:title", (req, res) => {
@@ -102,16 +112,20 @@ app.post("/users", (req, res) => {
 
 /*NOT INCLUDED*/
 // Get a user's data by username
-app.get("/users/:Username", (req, res) => {
-  Users.findOne({ Username: req.params.Username })
-    .then(user => {
-      res.json(user);
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
+app.get(
+  "/users/:Username",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Users.findOne({ Username: req.params.Username })
+      .then(user => {
+        res.json(user);
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 // Allow users to update their user info (username)
 app.put("/users/:Username", (req, res) => {
