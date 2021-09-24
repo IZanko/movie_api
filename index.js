9f1188209bbffdfd93d4d001606c76f1298367a2
IZanko <zankoiva@hotmail.com>
2021-09-24 13:10:08 -0400

    return json with info

--- index.js
+++ index.js
@@ -225,7 +225,7 @@ app.get(
   (req, res) => {
     Users.findOne({ Username: req.params.Username })
       .then(user => {
-        res.json(user.Username + " exists!");
+        res.json(user);
       })
       .catch(err => {
         console.error(err);
